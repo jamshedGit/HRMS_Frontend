a44abf2939f3719c550d43552617ec6df67cc2eb
Faaiz <faaiz@gmail.com>
2024-10-07 11:58:30 +0500

    done

--- src/app/modules/Salarypolicy/pages/index.js
+++ src/app/modules/Salarypolicy/pages/index.js
@@ -1,7 +1,7 @@
 import React, { Suspense } from "react";
 import { Redirect, Switch } from "react-router-dom";
 import { LayoutSplashScreen, ContentRoute } from "../../../../_metronic/layout";
-import { SalarypolicyPage } from "./banks/SalarypolicyPage";
+import { SalarypolicyPage } from "./salarypolicy/SalarypolicyPage";
 
 export default function SalarypolicyManagement() {
  
