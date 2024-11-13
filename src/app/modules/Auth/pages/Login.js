1f27033c6238971ffe4ba501013c689c6973c9d7
Faaiz <faaiz@gmail.com>
2024-11-13 17:46:08 +0500

    working on login

--- src/app/modules/Auth/pages/Login.js
+++ src/app/modules/Auth/pages/Login.js
@@ -6,6 +6,7 @@ import { connect, useSelector, shallowEqual, useDispatch } from "react-redux";
 import { FormattedMessage, injectIntl } from "react-intl";
 import * as auth from "../_redux/authRedux";
 import { login } from "../_redux/authCrud";
+import { toAbsoluteUrl } from "../../../../_metronic/_helpers";
 
 /*
   INTL (i18n) docs:
@@ -104,16 +105,44 @@ function Login(props) {
   });
 
   return (
-    <div className="login-form login-signin" id="kt_login_signin_form">
-      {/* begin::Head */}
-      <div className="text-center mb-10 mb-lg-20">
-        <h3 className="font-size-h1">
-          <FormattedMessage id="AUTH.LOGIN.TITLE" />
-        </h3>
-        <p className="text-muted font-weight-bold">
-          Enter your username and password
-        </p>
+    <div className=" login-signin" id="kt_login_signin_form"
+      style={{
+        height: "80vh",  // Full height of the viewport
+        width: "30vw",  // Make the form take full width
+        justifyContent: "center",  // Center horizontally
+        alignItems: "center",  // Center vertically
+        backgroundColor: "white",
+        backgroundSize: "cover",  // Ensures the gradient covers the entire area
+        backgroundPosition: "center",  // Ensures the background is centered
+        padding: "60px",  // Padding inside the form
+      }}
+
+    >
+
+
+      <div
+        className="text-center mb-10 mb-lg-20"
+        style={{
+          width: "70%",  // Make the form take full width
+          height: "7vh",  // 10% of the viewport height (can be adjusted)
+          backgroundImage: `url(${toAbsoluteUrl("/media/bg/login-logo.png")})`,  // Add image as background
+          backgroundSize: "cover",  // Ensure the image covers the entire container
+          backgroundPosition: "center",  // Center the image inside the container
+          backgroundRepeat: "no-repeat",  // Prevent the image from repeating
+          borderRadius: "8px",  // Optional: rounded corners
+
+        }}
+      >
+
       </div>
+
+      <div>
+        <h1 className="">
+          Login
+        </h1>
+      </div>
+      <br />
+      <br />
       {/* end::Head */}
 
       {/*begin::Form*/}
@@ -137,11 +166,18 @@ function Login(props) {
           ""
         )}
 
+
+
         <div className="form-group fv-plugins-icon-container">
           <input
+            style={{
+              backgroundColor: "white", // Custom background color
+
+
+            }}
             placeholder="Email"
             type="text"
-            className={`form-control form-control-solid h-auto py-5 px-6 ${getInputClasses(
+            className={`form-control form-control-solid h-auto py-5 px-6  ${getInputClasses(
               "email"
             )}`}
             name="emails"
@@ -153,6 +189,9 @@ function Login(props) {
             </div>
           ) : null}
         </div>
+
+        <br />
+
         <div className="form-group fv-plugins-icon-container">
           <input
             placeholder="Password"
@@ -169,26 +208,58 @@ function Login(props) {
             </div>
           ) : null}
         </div>
-        <div className="form-group d-flex flex-wrap justify-content-end align-items-center">
-          {/* <Link
-            to="/auth/forgot-password"
-            className="text-dark-50 text-hover-primary my-3 mr-2"
-            id="kt_login_forgot"
-          >
-            <FormattedMessage id="AUTH.GENERAL.FORGOT_BUTTON" />
-          </Link> */}
+        <br />
+
+        <div className="d-flex align-items-center  ">
+          <input
+            type="checkbox"
+            style={{
+              transform: "scale(1.5)",  // This will increase the size of the checkbox
+              height: "12px",  // You can also adjust height directly, though it's less effective
+              width: "12px",   // Increase width as well if needed
+            }}
+          />
+          <span className="ml-3" style={{ fontSize: '15px' }}>Remember me</span>
+        </div>
+        <br />
+        <br />
+
+        <div className="">
+
           <button
-            id="kt_login_signin_submit"
+            id=""
             type="submit"
             disabled={formik.isSubmitting}
-            className={`btn btn-primary font-weight-bold px-9 py-4 my-3`}
+            className="custom-button"
           >
             <span>Sign In</span>
             {loading && <span className="ml-3 spinner spinner-white"></span>}
           </button>
         </div>
+
+        <br />
+        <div className="d-flex align-items-center  ">
+
+          <span className="ml-3" style={{ fontSize: '15px', color: "#007bff" }}>Forgot Password?</span>
+        </div>
+
+
       </form>
+      {/* <div>footer</div> */}
       {/*end::Form*/}
+      <br/>
+      <br/>
+      
+      {/* <footer class="footer">
+        <div class="footer-container">
+            <div class="footer-left">
+                <p>Privacy Policy</p>
+                <p>Legal</p>
+                <p>Contact Us</p>
+            </div>
+           
+        </div>
+    </footer> */}
     </div>
   );
 }
