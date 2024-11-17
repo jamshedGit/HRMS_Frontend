b1a13950486f2d76c08161767fa21e3b0bb176af
jamshedGit <jamshed@dynasoftcloud.com>
2024-11-17 23:31:15 +0500

    fixes in random forms

--- src/app/modules/Employee_Salary_Setup/pages/banks/bank-edit-dialog/BankEditDialogHeader.js
+++ src/app/modules/Employee_Salary_Setup/pages/banks/bank-edit-dialog/BankEditDialogHeader.js
@@ -31,7 +31,7 @@ export function BankEditDialogHeader({ id, isUserForRead }) {
       {actionsLoading && <ModalProgressBar />}
       <Modal.Header closeButton>
         <Modal.Title id="example-modal-sizes-title-lg">
-          {!isUserForRead ? title : "View Earning"}
+          {!isUserForRead ? title : "View Employee Salary"}
         </Modal.Title>
       </Modal.Header>
     </>
