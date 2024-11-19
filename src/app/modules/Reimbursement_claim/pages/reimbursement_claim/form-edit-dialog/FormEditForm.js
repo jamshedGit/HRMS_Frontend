a53a29433d0fae56d77432e19ac8d8278c39a514
Faaiz <faaiz@gmail.com>
2024-11-19 14:26:28 +0500

    done

--- src/app/modules/Reimbursement_claim/pages/reimbursement_claim/form-edit-dialog/FormEditForm.js
+++ src/app/modules/Reimbursement_claim/pages/reimbursement_claim/form-edit-dialog/FormEditForm.js
@@ -281,6 +281,7 @@ export function FormEditForm({
                       {isFileReq && <span style={{ color: "red" }}>*</span>}
                     </label>
                     <input
+  
                       name="file"
                       type="file"
                       accept=".jpeg,.jpg,.png,.pdf,.doc,.docx"
--- src/app/modules/Reimbursement_claim/pages/reimbursement_claim/form-table/FormTable.js
+++ src/app/modules/Reimbursement_claim/pages/reimbursement_claim/form-table/FormTable.js
@@ -192,7 +192,7 @@ export function FormTable() {
       <Card>
         <Card.Header className="d-flex justify-content-center">
         <div className='accordion-header-btn w-100  d-flex justify-content-center'>
-          <Accordion.Toggle as={Button} eventKey="0" >
+          <Accordion.Toggle  eventKey="0" className="flex-between-center text-bold">
             Reimbursement Claim Details
             <KeyboardArrowDown />
           </Accordion.Toggle>
