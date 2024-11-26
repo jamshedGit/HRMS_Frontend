28cab68a4e9a8e1d2d99bc425bd465426d9a2e79
Faaiz <faaiz@gmail.com>
2024-11-26 11:59:08 +0500

    done

--- src/app/modules/Payroll_Process/pages/payroll_process/form-table/FormTable.js
+++ src/app/modules/Payroll_Process/pages/payroll_process/form-table/FormTable.js
@@ -166,7 +166,7 @@ export function FormTable() {
         <Card.Header className="d-flex justify-content-center">
         <div className='accordion-header-btn w-100  d-flex justify-content-center'>
           <Accordion.Toggle as={Button} eventKey="0" >
-            Reimbursement Claim Details
+            Payroll Process Details
             <KeyboardArrowDown />
           </Accordion.Toggle>
           </div>
