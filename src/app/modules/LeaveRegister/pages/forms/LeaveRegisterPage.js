b1f5a88308df9539cb370ff9565f4066e3e16ed6
Muhammad Ahmed <ahmed.habib@dynasoftcloud.com>
2024-12-26 10:07:11 +0500

    remove unused module imports

--- src/app/modules/LeaveRegister/pages/forms/LeaveRegisterPage.js
+++ src/app/modules/LeaveRegister/pages/forms/LeaveRegisterPage.js
@@ -4,7 +4,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { Card } from "react-bootstrap";
 import { FormTable } from "./form-table/FormTable";
 import { FormFilter } from "./form-filter/FormFilter";
-import { fetchAllActiveEmployees, fetchAllDept, fetchAllFormsMenu, fetchAllSubsidiaryData } from "../../../../../_metronic/redux/dashboardActions";
+import { fetchAllDept, fetchAllFormsMenu, fetchAllSubsidiaryData } from "../../../../../_metronic/redux/dashboardActions";
 import { useSelector, useDispatch } from "react-redux";
 import { FormUIProvider } from "./FormUIContext";
 import { CardHeader } from "../../../../../_metronic/_partials/controls";
