1ec16dee2843ab0179317fd3fce558bf5ac7b0f0
Muhammad Ahmed <ahmed.habib@dynasoftcloud.com>
2025-01-23 11:12:40 +0500

    Logout on Token Expire Logic

--- src/redux/setupAxios.js
+++ src/redux/setupAxios.js
@@ -7,7 +7,6 @@ export default function setupAxios(axios, store) {
       } = store.getState();
 
       if (tokens) {
-       
         config.headers.Authorization = `Bearer ${tokens?.access}`;
       }
 
@@ -16,4 +15,28 @@ export default function setupAxios(axios, store) {
     (err) => Promise.reject(err)
   );
   // }, 10000)
+
+
+  // Add response interceptor
+  axios.interceptors.response.use(
+    (response) => {
+      // Return response as is if status code is in the 2xx range
+      return response;
+    },
+    (error) => {
+      if (error.response) {
+        // Check for specific status code
+        const { status } = error.response;
+
+        if (status === 308) {
+          // Redirect to login page If token is expired or not logged in
+          localStorage.clear()
+          window.location.href = '/auth/login'
+        } 
+      }
+
+      // Reject the error for further handling in individual requests
+      return Promise.reject(error);
+    }
+  );
 }
